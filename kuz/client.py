350555cb8d82e31bd14c7b01f50c15a57a163138
Manh Dinh <mdinh@uwaterloo.ca>
2024-01-17 16:42:46 -0500

    modify script for memory monitor

--- kuz/client.py
+++ kuz/client.py
@@ -1,21 +1,22 @@
 import time
 import psutil
-from timeout_decorator import timeout, TimeoutError
 import sys
 import kuzu
 import logging
 
-@timeout(600)  # Set the timeout to 10 minutes (600 seconds)
-def run_query(conn, system_variant, sf, query_id, query_spec, results_file):
+def run_query(conn, numThreads, sf, query_id, query_spec, results_file):
     start_time = time.time()
 
     # Monitor initial system usage
     initial_ram_usage = psutil.virtual_memory().used
     initial_disk_usage = psutil.disk_usage('/').used
 
-    results = conn.execute(query_spec)
-    if results.has_next():
-        result = results.get_next()
+    try:
+        results = conn.execute(query_spec)
+        if results.has_next():
+            result = results.get_next()
+    except RuntimeError:
+        return
 
     end_time = time.time()
     duration = end_time - start_time
@@ -26,33 +27,38 @@ def run_query(conn, system_variant, sf, query_id, query_spec, results_file):
 
     ram_change = final_ram_usage - initial_ram_usage
     disk_change = final_disk_usage - initial_disk_usage
+    
+    results_file.write(f"KuzuDB\t{query_id}\t{numThreads} threads\t{sf}\t{duration:.4f}\t{result[0]}\t")
+    results_file.write(f"{ram_change / (1024 ** 3):.2f} GB\n")
+    results_file.flush()
 
-    return duration, result
+    return (duration, result)
 
 
 def main():
-    if len(sys.argv) < 1:
+    if len(sys.argv) < 2:
         print("Usage: client.py sf")
         print("Where sf is the scale factor)")
         exit(1)
     else:
         sf = sys.argv[1]
+    
+    if len(sys.argv) > 2:
+        numThreads = int(sys.argv[2])
+    else:
+        numThreads = 4
 
     db = kuzu.Database('kuz/scratch/lsqb-database')
-    conn = kuzu.Connection(db)
+    conn = kuzu.Connection(db, num_threads=numThreads)
+    conn.set_query_timeout(600000) # Set timeout to 10 minutes
 
-    with open(f"results/kuzudb_results.csv", "a+") as results_file:
+    with open(f"results/results.csv", "a+") as results_file:
         for i in range(1, 10):
+            if sf != "example" and float(sf) >= 1 and i == 3:
+                continue
             print(i)
             with open(f"kuz/q{i}.cypher", "r") as query_file:
-                try:
-                    runtime, result = run_query(conn, "", sf, i, query_file.read(), results_file)
-                except TimeoutError:
-                    results_file.write(f"{i},\tkuz/q{i}.cypher,\t{sf},\t,\t,\tTimeout,\tTime limit is set to 10 minutes\n")
-                    results_file.flush()
-                else:
-                    results_file.write(f"{i},\tkuz/q{i}.cypher,\t{sf},\t{runtime:4f},\t{result[0]},\tSuccess,\t\n")
-                    results_file.flush()
+                run_query(conn, numThreads, sf, i, query_file.read(), results_file)
 
 if __name__ == "__main__":
     logging.basicConfig(format='%(process)d-%(levelname)s-%(message)s', level=logging.DEBUG)
